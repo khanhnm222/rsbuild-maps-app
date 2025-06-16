c5fbb7f1f4f7d59453a327026c85adebc8875615
Nguyen Minh Khanh <khanhnm222@gmail.com>
2025-06-16 10:35:00 +0700

    feat: setup shadcn ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css';
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+import './globals.css';
 
 const App = () => {
   return (
     <div className="content flex justify-center h-full items-center flex-col">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <div className="flex flex-row gap-2">
+        <Input type="email" placeholder="Email" />
+        <Button>Button</Button>
+      </div>
     </div>
   );
 };
