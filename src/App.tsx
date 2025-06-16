2fda49da1d58ce559f3040c01518d1f48bec093f
Nguyen Minh Khanh <khanhnm222@gmail.com>
2025-06-16 10:18:27 +0700

    feat: add tailwind css for ui styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="content">
+    <div className="content flex justify-center h-full items-center flex-col">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
