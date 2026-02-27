eea28fb73867e22abb5ab8ed96ac883d1242fbe0
Nguyen Minh Khanh <khanhnm222@gmail.com>
2026-02-27 21:17:38 +0700

    feat: update map view component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { RouterProvider } from 'react-router-dom';
 import router from './routers/router';
 import './globals.css';
 
-const App = () => {
+function App() {
   return (
     <RouterProvider router={router} />
   );
-};
+}
 
 export default App;
--- src/components/pages/Map/Map.tsx
+++ src/components/pages/Map/Map.tsx
@@ -1,15 +1,9 @@
-import { Button } from "../../atoms/ui/button";
-import { Input } from "../../atoms/ui/input";
+import MapView from "../../molecules/MapView/MapView";
 
 const Map = () => {
   return (
     <div className="content flex justify-center h-full items-center flex-col">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-      <div className="flex flex-row gap-2">
-        <Input type="email" placeholder="Email" />
-        <Button>Button</Button>
-      </div>
+      <MapView />
     </div>
   );
 };
